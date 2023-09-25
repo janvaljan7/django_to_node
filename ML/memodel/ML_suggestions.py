ef3cc11c3daa41e2beb1c0c10b150c105e10bcbf
mahsa-ashna <81348960+mahsa-ashna@users.noreply.github.com>
2023-09-25 17:52:36 +0330

    Mock Server with postman

--- ML/memodel/ML_suggestions.py
+++ ML/memodel/ML_suggestions.py
@@ -1,10 +1,14 @@
-import pandas as pd 
-import numpy as np 
+# import pandas as pd 
+# import numpy as np 
+import requests
+from django.http import HttpResponse
+from rest_framework import status
 
-def ML(first_choice, second_choice=None, third_choice=None):
-    first = str(first_choice).lower()
-    second = str(second_choice).lower()
-    third = str(third_choice).lower()
+def ML():
+    
+    # first = str(first_choice).lower()
+    # second = str(second_choice).lower()
+    # third = str(third_choice).lower()
     dictionary = {
         "history": "Statue of Liberty and Ellis Island\n Federal Hall\n New-York Historical Society\n",
         "kid-friendly": "Central Park (playgrounds, Central Park Zoo)\nAmerican Museum of Natural History\nBronx Zoo\n",
@@ -15,4 +19,22 @@ def ML(first_choice, second_choice=None, third_choice=None):
         "architercture": "Empire State Building\nFlatiron Building\nGrand Central Terminal\n",
         "food and culinary places": "Chelsea Market\nSmorgasburg (food market)\nKatz's Delicatessen\n"
     }   
-    return dictionary[first]
+    node_backend_url = "https://7bf69800-c806-4fc1-a8ca-937e2963fcd2.mock.pstmn.io"
+
+    response = requests.get(node_backend_url)
+    if response:
+        data = response.json()
+        
+        first = str(data['first']).lower()
+        # second = str(data['second']).lower()
+        # third = str(data['third']).lower()
+    
+        result = dictionary[first]
+        print(result)
+        return result
+    else:
+        return None
+        
+
+
+
--- ML/memodel/views.py
+++ ML/memodel/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 import subprocess   
 from .serializers import interestSerializer
 
+from memodel.ML_suggestions import ML
 
 
 class getinfo(APIView):
@@ -16,10 +17,15 @@ class getinfo(APIView):
         #     first = serializer.validated_data['first']
         #     second = serializer.validated_data['second']
         #     third = serializer.validated_data['third']
-        first = request.data.get('first')
-        second = request.data.get('second')
-        third = request.data.get('third')
+        # first = request.data.get('first')
+        # second = request.data.get('second')
+        # third = request.data.get('third')
 
-        subprocess.call(['python', './ML_suggestions.py', first, second, third])
-        result = subprocess.run(['python', './ML_suggestions.py'], capture_output=True)
-        return HttpResponse(f'Suggestion: {result}')
+        suggestion = ML()
+        print(suggestion)
+        suggestion_html = suggestion.replace('\n', '<br>')
+        return HttpResponse(f'Suggestion: <br>{suggestion_html}', content_type="text/html")
+
+        # subprocess.call(['python', './ML_suggestions.py', first, second, third])
+        # result = subprocess.run(['python', './ML_suggestions.py'], capture_output=True)
+        # return HttpResponse(f'Suggestion: {suggestion}')
